3369b1f3f42e781cffdeedaba22633762aa396b2
kassings <s.a.kassing@gmail.com>
2020-04-04 01:47:06 +0200

    Remove the 10G from the examples sh

--- quick/run_examples_basic_sim.sh
+++ quick/run_examples_basic_sim.sh
@@ -1,8 +1,8 @@
 cd ../simulator || exit 1
 
-for experiment in "example_single" "example_single_many_small_flows" "example_single_one_10g" "example_single_one_large_flow" \
+for experiment in "example_single" "example_single_many_small_flows" "example_single_one_large_flow" \
                   "example_ring" "example_leaf_spine" "example_leaf_spine_servers" "example_fat_tree_k4_servers" \
-                  "example_big_grid" "example_big_grid_one_flow"
+                  "example_big_grid" "example_big_grid_one_flow"  #  Too long in debug: "example_single_one_10g"
 do
   rm -rf ../runs/${experiment}/logs
   mkdir ../runs/${experiment}/logs
