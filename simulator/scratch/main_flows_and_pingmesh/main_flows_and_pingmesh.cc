58bb3c600cd04fcd02ef1868870b918cfde35ca1
kassings <s.a.kassing@gmail.com>
2020-04-23 17:55:54 +0200

    Add a main for flows AND pingmesh

--- simulator/scratch/main_flows_and_pingmesh/main_flows_and_pingmesh.cc
+++ simulator/scratch/main_flows_and_pingmesh/main_flows_and_pingmesh.cc
@@ -11,6 +11,7 @@
 #include <chrono>
 #include <stdexcept>
 #include "ns3/basic-simulation.h"
+#include "ns3/flow-scheduler.h"
 #include "ns3/pingmesh-scheduler.h"
 
 using namespace ns3;
@@ -23,19 +24,22 @@ int main(int argc, char *argv[]) {
     // Retrieve run directory
     CommandLine cmd;
     std::string run_dir = "";
-    cmd.Usage("Usage: ./waf --run=\"main_pingmesh --run_dir='<path/to/run/directory>'\"");
+    cmd.Usage("Usage: ./waf --run=\"main_flows_and_pingmesh --run_dir='<path/to/run/directory>'\"");
     cmd.AddValue("run_dir",  "Run directory", run_dir);
     cmd.Parse(argc, argv);
     if (run_dir.compare("") == 0) {
-        printf("Usage: ./waf --run=\"main_pingmesh --run_dir='<path/to/run/directory>'\"");
+        printf("Usage: ./waf --run=\"main_flows_and_pingmesh --run_dir='<path/to/run/directory>'\"");
         return 0;
     }
 
     // Start the simulation using this run directory
     BasicSimulation simulation(run_dir);
+    FlowScheduler flowScheduler(&simulation); // Requires filename_schedule to be present in the configuration
+    flowScheduler.Schedule();
     PingmeshScheduler pingmeshScheduler(&simulation); // Requires pingmesh_interval_ns to be present in the configuration
     pingmeshScheduler.Schedule();
     simulation.Run();
+    flowScheduler.WriteResults();
     pingmeshScheduler.WriteResults();
     simulation.Finalize();
 
