e391d291c5d355625ce602bf60d8ac72ce0f225c
kassings <s.a.kassing@gmail.com>
2020-05-06 18:52:22 +0200

    Add GetNumNodes() to topology abstract

--- simulator/src/basic-sim/model/topology.h
+++ simulator/src/basic-sim/model/topology.h
@@ -11,6 +11,7 @@ class Topology : public Object
 public:
     static TypeId GetTypeId (void);
     virtual const NodeContainer& GetNodes() = 0;
+    virtual int64_t GetNumNodes() = 0;
     virtual bool IsValidEndpoint(int64_t node_id) = 0;
     virtual const std::set<int64_t>& GetEndpoints() = 0;
 };
