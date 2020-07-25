49583dcab440bdc05ba61d3672b1a2da6e1b6b44
Hanjing Gao <hgao10@student.ethz.ch>
2020-07-25 13:53:32 -0700

    refactored and streamlined plotting functions

--- simulator/src/basic-apps/helper/horovod_worker_plot.py
+++ simulator/src/basic-apps/helper/horovod_worker_plot.py
@@ -3,18 +3,9 @@ import collections
 import matplotlib.pyplot as plt
 import pathlib
 import numpy as np
-
+import argparse
 # NS_LOG="*" ./waf --run="main_horovod --run_dir='/mnt/raid10/hanjing/thesis/ns3/ns3-basic-sim/runs/horovod_example'" > ../runs/horovod_example/simulation_logs
-# print(sys.argv[0])
-file_name = sys.argv[1]
-print(file_name)
-file_path = pathlib.Path(file_name)
-log_dir = file_path.resolve().parent
-print(f"file_path.parent: {file_path.resolve().parent}")
-available_prio_samples_files = []
-file_header = ""
-num_layers = 0
-max_time_ns = 0
+
 class Event():
     def __init__(self, iter, layer, name, timestamp_ns):
         self.event_name = name
@@ -24,104 +15,107 @@ class Event():
     def __str__(self):
         return f"event_name: {self.event_name}, time_ns: {self.time}, iter: {self.iter}"
 
-event_dict = collections.defaultdict(list) # key: layer index, value: (event_name, time_ns)  
-timeline_event = collections.defaultdict(list) #key: layer index, value: duration
-iter_event_dict = collections.defaultdict(list) # key: iteration index, value: event_dict
-with open(file_name, "r") as inputfile:
-    line = inputfile.readline().strip("\n")
-    file_header = line
-    print(f"header: {file_header}")
-    # print(line, end="")
-    i = 0 
-    while line !='':
-        i += 1
+
+def construct_event_list_from_progress_file(progress_file):
+    event_dict = collections.defaultdict(list) # key: layer index, value: (event_name, time_ns)  
+    max_time_ns = 0
+    with open(progress_file, "r") as inputfile:
+        # read in first line which is the header 
+        # in the form of Iteration_idx,Layer_idx,Event,Time
+        line = inputfile.readline().strip("\n")
+
         # print(line, end="")
-        line=inputfile.readline().strip("\n")
-        # data
-        line = line.split(",")
-        if(line == ['']):
-            break
-        # print(f"line: {line}")
-        iter = int(line[0])
-        layer = int(line[1])
-        event_name = line[2]
-        time_ns = int(line[3])
-        max_time_ns = max(max_time_ns, time_ns)
-        # print(f"layer: {layer}, event_name: {event_name}, time_ns: {time_ns}")
-        e = Event(iter, layer, event_name, time_ns)
-        event_dict[layer].append(e)
-        
-        print(f"added event: {e}")
+        i = 0 
+        while line !='':
+            i += 1
+            # print(line, end="")
+            line=inputfile.readline().strip("\n")
+            # data
+            line = line.split(",")
+            if(line == ['']):
+                break
+            # print(f"line: {line}")
+            iter = int(line[0])
+            layer = int(line[1])
+            event_name = line[2]
+            time_ns = int(line[3])
+            max_time_ns = max(max_time_ns, time_ns)
+            # print(f"layer: {layer}, event_name: {event_name}, time_ns: {time_ns}")
+            e = Event(iter, layer, event_name, time_ns)
+            event_dict[layer].append(e)
+            
+            print(f"added event: {e}")
+    
+    num_layers = max(event_dict.keys())+1
+    print(f"num_layers: {num_layers}, max_time_ns: {max_time_ns}")
+    return event_dict, num_layers, max_time_ns
 
-num_layers = max(event_dict.keys())+1
-print(f"num_layers: {num_layers}")
-duration = 0
-bp_start_time = 0
-fp_start_time = 0
-send_start_time = 0
-s1 = "Start_Sending_Partition"
-s1_len = len(s1)
-s2 = "Received_Partition"
-s2_len = len(s2)
-for priority in [0, 2, 6]:
-    sample_f = pathlib.Path(f"{log_dir}/Priority_{priority}_samples")
-    if sample_f.is_file():
-        # remove file if exits
-        sample_f.unlink()
+def construct_timeline_and_priority_events_from_event_list(event_dict, log_dir):
+    timeline_event = collections.defaultdict(list) #key: layer index, value: duration
+    duration = 0
+    bp_start_time = 0
+    fp_start_time = 0
+    send_start_time = 0
 
-priority_duration_timestamp = collections.defaultdict(list)
-for key, event_list in event_dict.items():
-    # first_start_sending_partition= True
-    for e in event_list:
-        # print(f"e in event_list: {e}")
-        if e.event_name == "BP_Start" :
-            bp_start_time = e.time
-            first_start_sending_partition= True
+    for priority in [0, 2, 6]:
+        sample_f = pathlib.Path(f"{log_dir}/Priority_{priority}_samples")
+        if sample_f.is_file():
+            # remove file if exits
+            sample_f.unlink()
 
-        elif e.event_name == "FP_Start":
-            fp_start_time = e.time
-        elif e.event_name[:5] == "Start" and first_start_sending_partition:
-            send_start_time = e.time
-            first_start_sending_partition = False
-        elif e.event_name == "BP_Done":
-            duration = int(e.time) - int(bp_start_time)
-            timeline_event[key].append((bp_start_time, duration))
-            print(f"layer: {key}, iter: {e.iter} Event name: {e.event_name}: duration: {duration} ")
-        elif e.event_name == "FP_Done":
-            duration = int(e.time) - int(fp_start_time)
-            timeline_event[key].append((fp_start_time, duration))
-            print(f"layer: {key} Event name: {e.event_name}: duration: {duration} ")
-                
-        elif e.event_name[:5] == "Recei":
-            duration = int(e.time) - int(send_start_time)
-            prio = e.event_name.split("_")[-1]
-            # prio_sample_file = pathlib.Path(f"{log_dir}/Priority_{prio}_samples")
-            # if prio_sample_file not in available_prio_samples_files:
-            #     available_prio_samples_files.append(prio_sample_file)
-            # print(f"prio_sample_file: exists: {prio_sample_file.is_file()}")
-            # with open(f"{log_dir}/Priority_{prio}_samples", "a") as sample_file:
-            #     sample_file.write(f"{duration},{send_start_time}\n")
-            priority_duration_timestamp[prio].append([int(duration), int(send_start_time)])
+    priority_duration_timestamp = collections.defaultdict(list)
+    for key, event_list in event_dict.items():
+        # first_start_sending_partition= True
+        for e in event_list:
+            # print(f"e in event_list: {e}")
+            if e.event_name == "BP_Start" :
+                bp_start_time = e.time
+                first_start_sending_partition= True
 
-            timeline_event[key].append((send_start_time, duration))
-            send_start_time = e.time
-            print(f"layer: {key} Event name: {e.event_name}: duration: {duration} ")
+            elif e.event_name == "FP_Start":
+                fp_start_time = e.time
+            elif e.event_name[:5] == "Start" and first_start_sending_partition:
+                send_start_time = e.time
+                first_start_sending_partition = False
+            elif e.event_name == "BP_Done":
+                duration = int(e.time) - int(bp_start_time)
+                timeline_event[key].append((bp_start_time, duration))
+                print(f"layer: {key}, iter: {e.iter} Event name: {e.event_name}: duration: {duration} ")
+            elif e.event_name == "FP_Done":
+                duration = int(e.time) - int(fp_start_time)
+                timeline_event[key].append((fp_start_time, duration))
+                print(f"layer: {key} Event name: {e.event_name}: duration: {duration} ")
+                    
+            elif e.event_name[:5] == "Recei":
+                duration = int(e.time) - int(send_start_time)
+                prio = e.event_name.split("_")[-1]
+                priority_duration_timestamp[prio].append([int(duration), int(send_start_time)])
 
-def write_to_priority_sample_files():
+                timeline_event[key].append((send_start_time, duration))
+                send_start_time = e.time
+                print(f"layer: {key} Event name: {e.event_name}: duration: {duration} ")
+    
+    return timeline_event, priority_duration_timestamp
+
+def write_to_priority_sample_files(priority_duration_timestamp,log_dir):
+    available_prio_samples_files = []
     for prio, duration_stamp_pairs in priority_duration_timestamp.items():
-        # sorted by timestamp
+        # sorted by timestamp in ns
         duration_stamp_pairs.sort(key= lambda x:x[1])
         prio_sample_file = pathlib.Path(f"{log_dir}/Priority_{prio}_samples")
         if prio_sample_file not in available_prio_samples_files:
             available_prio_samples_files.append(prio_sample_file)
         with open(f"{log_dir}/Priority_{prio}_samples", "a") as sample_file:
-            for dur, time in duration_stamp_pairs:
-                if time >= 2000000000:
-                    sample_file.write(f"{dur/1000000:.2f},{time/1000000000:.1f}\n")        
+            for dur_ms, time_s in duration_stamp_pairs:
+                if time_s >= 2000000000:
+                    sample_file.write(f"{dur_ms/1000000:.2f},{time_s/1000000000:.1f}\n")  
+    return available_prio_samples_files      
 
-write_to_priority_sample_files()
 
-def plot_horovod_worker_event_progress():
+def plot_horovod_worker_event_progress(timeline_event, num_layers, max_time_ns):
+    # num_layers = max(event_dict.keys())+1
+    # print(f"num_layers: {num_layers}")
+
     # Plot the timeline
     fig, ax = plt.subplots()
 
@@ -216,5 +210,29 @@ def plot_prio_samples(sample_file_list):
         ax.set_title(f"Priority {translated_prio} Histogram")        
         plt.show(block=True)
 
-plot_horovod_worker_event_progress()
-plot_prio_samples(available_prio_samples_files)
+if __name__ == "__main__":
+    # file_name = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("run_dir", help="specify absolute path of the run directory")
+    # parser.add_argument("--run_program", action="store_true", help="execute the simulation specified by the main program")
+    # parser.add_argument("--plot_utilization", help="plot the utilization of the links", choices=["hist","line"])
+    args = parser.parse_args()
+    run_dir_path_absolute = pathlib.Path(args.run_dir)
+    if run_dir_path_absolute.exists == False:
+        raise RuntimeError("Please enter an valid path for run directory")
+        # print(f"Please enter an valid path for run directory")
+        
+
+    log_dir = run_dir_path_absolute/"logs_ns3"
+
+    progress_file = log_dir/"HorovodWorker_0_layer_50_port_1024_progress.txt"
+    event_dict, num_layers, max_time_ns = construct_event_list_from_progress_file(progress_file)
+
+    timeline_event, priority_duration_timestamp = construct_timeline_and_priority_events_from_event_list(event_dict, log_dir)
+    plot_horovod_worker_event_progress(timeline_event, num_layers, max_time_ns)
+
+    available_prio_samples_files = write_to_priority_sample_files(priority_duration_timestamp, log_dir)
+    plot_prio_samples(available_prio_samples_files)
+# plot_horovod_worker_event_progress()
+# plot_prio_samples(available_prio_samples_files)
+# write_to_priority_sample_files()
